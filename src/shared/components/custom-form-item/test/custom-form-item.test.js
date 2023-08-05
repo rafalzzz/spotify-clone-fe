aaa4ca997df839bc643f4029406873414e06b89c
rafalzzz <rzdeb94@gmail.com>
2023-08-05 15:53:44 +0200

    remove unused imports

--- src/shared/components/custom-form-item/test/custom-form-item.test.js
+++ src/shared/components/custom-form-item/test/custom-form-item.test.js
@@ -1,4 +1,4 @@
-import { render, screen, fireEvent, act, waitFor } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { Form } from 'antd';
 import React from 'react';
