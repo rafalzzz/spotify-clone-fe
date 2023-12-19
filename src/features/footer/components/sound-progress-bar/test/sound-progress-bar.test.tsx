ae0242a59ca764f7ee438ae1764a01a9d45641a7
rafalzzz <rzdeb94@gmail.com>
2023-12-20 00:46:39 +0100

    remove unused imports #2

--- src/features/footer/components/sound-progress-bar/test/sound-progress-bar.test.tsx
+++ src/features/footer/components/sound-progress-bar/test/sound-progress-bar.test.tsx
@@ -1,4 +1,4 @@
-import { render, fireEvent, screen } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
 
